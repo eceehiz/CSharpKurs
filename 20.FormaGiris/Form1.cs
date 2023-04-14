593006855b315935bc8998855be530a1f00fc305
Ece <120506489+eceehiz@users.noreply.github.com>
2023-04-14 16:44:23 +0300

    Add files via upload

---
+++ 20.FormaGiris/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20.FormaGiris
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Red;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Blue;
+        }
+
+        private void btnEkle2_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Green;
+        }
+
+        
+
+
+
+
+
+
+    }
+}
