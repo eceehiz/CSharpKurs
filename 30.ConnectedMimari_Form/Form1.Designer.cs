2025fd5386495cd26323820a54851fe7253f332d
Ece <120506489+eceehiz@users.noreply.github.com>
2023-04-26 15:59:50 +0300

    Add files via upload

---
+++ 30.ConnectedMimari_Form/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace _30.ConnectedMimari_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstUrunler = new System.Windows.Forms.ListBox();
+            this.lstKategoriler = new System.Windows.Forms.ListBox();
+            this.lstTedarikciler = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lstUrunler
+            // 
+            this.lstUrunler.FormattingEnabled = true;
+            this.lstUrunler.ItemHeight = 16;
+            this.lstUrunler.Location = new System.Drawing.Point(21, 23);
+            this.lstUrunler.Name = "lstUrunler";
+            this.lstUrunler.Size = new System.Drawing.Size(391, 260);
+            this.lstUrunler.TabIndex = 0;
+            // 
+            // lstKategoriler
+            // 
+            this.lstKategoriler.FormattingEnabled = true;
+            this.lstKategoriler.ItemHeight = 16;
+            this.lstKategoriler.Location = new System.Drawing.Point(439, 23);
+            this.lstKategoriler.Name = "lstKategoriler";
+            this.lstKategoriler.Size = new System.Drawing.Size(169, 260);
+            this.lstKategoriler.TabIndex = 1;
+            // 
+            // lstTedarikciler
+            // 
+            this.lstTedarikciler.FormattingEnabled = true;
+            this.lstTedarikciler.ItemHeight = 16;
+            this.lstTedarikciler.Location = new System.Drawing.Point(665, 23);
+            this.lstTedarikciler.Name = "lstTedarikciler";
+            this.lstTedarikciler.Size = new System.Drawing.Size(325, 260);
+            this.lstTedarikciler.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 560);
+            this.Controls.Add(this.lstTedarikciler);
+            this.Controls.Add(this.lstKategoriler);
+            this.Controls.Add(this.lstUrunler);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstUrunler;
+        private System.Windows.Forms.ListBox lstKategoriler;
+        private System.Windows.Forms.ListBox lstTedarikciler;
+    }
+}
+
---
+++ 30.ConnectedMimari_Form/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _30.ConnectedMimari_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection();  //sql e bağlandık
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand(baglanti.ConnectionString = "Server=localhost; Database=Northwind; User=sa; Password=Password1");  //komut yazmak için nesne oluşturuyorum
+            
+            UrunListele();
+            KategoriListele();
+            TedarikcileriListele();
+
+        }
+        private void UrunListele()
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "select * from Urunler";  //komutu yazdık. bu sorguyu consolda bir üstteki parantezin içinde yazmıştık, bu sefer tek satırda yapmadık böyle yaptık
+
+            cmd.Connection = baglanti;
+            baglanti.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())  //ilk satırın tamamını okuyo
+            {
+                string adi = reader["UrunAdi"].ToString();
+                string fiyat = reader["BirimFiyati"].ToString();
+                string stok = reader["HedefStokDuzeyi"].ToString();
+                lstUrunler.Items.Add(string.Format("{0}-{1}-{2}", adi, fiyat, stok));
+            }
+
+            baglanti.Close();
+        }
+
+        private void KategoriListele()
+        {
+            SqlCommand cmd = new SqlCommand("select * from Kategoriler", baglanti);
+            if (baglanti.State == ConnectionState.Closed)
+            {
+                baglanti.Open();
+            }
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                lstKategoriler.Items.Add(reader[1].ToString());
+            }
+            baglanti.Close();
+        }
+
+        private void TedarikcileriListele()
+        {
+            SqlCommand cmd = new SqlCommand("select * from Tedarikciler", baglanti);
+            if (baglanti.State == ConnectionState.Closed)
+            {
+                baglanti.Open();
+            }
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                lstTedarikciler.Items.Add(reader[1].ToString());
+            }
+            baglanti.Close();
+        }
+    }
+}
